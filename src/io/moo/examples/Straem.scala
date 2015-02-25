3ed61fd4290437ecc54db66c4ba33a7ea7a14a3e
Erhan Bagdemir <erhan.bagdemir@googlemail.com>
2015-02-26 00:07:46 +0100

    straem object updated with workbench changes

--- src/io/moo/examples/Straem.scala
+++ src/io/moo/examples/Straem.scala
@@ -9,11 +9,28 @@ object Straem {
 
     def toList(): List[A] = {
       @annotation.tailrec
-      def toList(s: Stream[A], l: List[A]): List[A] = s match {
-        case Empty => Nil
-        case Cons(head, tail) => toList(tail(), head() :: l)
+      def toList(s: Stream[A], acc: List[A]): List[A] = s match {
+        case Empty => acc
+        case Cons(head, tail) => toList(tail(), head() :: acc)
       }
-      toList(this, Nil)
+      toList(this, Nil) reverse
+    }
+
+    def take(n: Int): Stream[A] = this match {
+      case Cons(head, tail) if n > 0 => Cons(() => head(), () => tail().take(n - 1))
+      case _ => Empty
+    }
+
+    def drop(n: Int): Stream[A] = this match {
+      case Cons(head, tail) if n <= 0 => Cons(() => head(), () => tail().drop(n - 1))
+      case Cons(_, tail) => tail().drop(n - 1)
+      case _ => Empty
+    }
+
+    def takeWhile(p: A => Boolean): Stream[A] = this match {
+      case Cons(head, tail) if p(head()) => Cons(() => head(), () => tail().takeWhile(p))
+      case Cons(_, tail) => tail().takeWhile(p)
+      case _ => Empty
     }
   }
 
@@ -30,9 +47,7 @@ object Straem {
     def empty[A]: Stream[A] = Empty
 
     def apply[A](as: A*): Stream[A] = {
-      println("apply")
       if (as.isEmpty) empty else cons(as.head, apply(as.tail: _*))
     }
   }
-
 }
